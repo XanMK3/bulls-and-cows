83458a9503008b4674b5252e679279feddfca4ed
Alexander Novikov <a.e.novikov.mk3@gmail.com>
2017-09-06 12:52:34 +0300

    include svg icons from sprite file

--- src/js/components/board.jsx
+++ src/js/components/board.jsx
@@ -13,9 +13,7 @@ function Board(props) {
             )}</ul>
             {props.readOnly ? <Result {...props.result} /> :
             <button type='button' className='guess-panel__btn' onClick={props.onSubmit}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                    <polyline points="20 6 9 17 4 12" />
-                </svg>
+                <svg className='svg-icon'><use xlinkHref="assets/sprite.svg#check"></use></svg>
             </button>}
         </div>
     )
--- src/js/components/menu.jsx
+++ src/js/components/menu.jsx
@@ -7,10 +7,7 @@ function Menu(props) {
     return <div className={cn('menu', { 'menu--open': props.open || !props.allowClose })}>
         <header className='menu__header'>
             {props.allowClose ? <button type='button' className='icon' onClick={props.close}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                    <line x1="18" y1="6" x2="6" y2="18"></line>
-                    <line x1="6" y1="6" x2="18" y2="18"></line>
-                </svg>
+                <svg className='svg-icon'><use xlinkHref="assets/sprite.svg#x"></use></svg>
             </button> : null}
         </header>
         <div className='menu__body'>
