c037b0aae6127353c43655698d7ec43ec9096934
Alexander Novikov <a.e.novikov.mk3@gmail.com>
2017-09-09 15:52:54 +0300

    change balls colors

--- src/js/const/index.js
+++ src/js/const/index.js
@@ -10,4 +10,5 @@ export const ITEM_TYPES = {
     BALL: 'ball'
 };
 
-export const COLORS = ['red', 'yellow', 'green', 'blue', 'orange', 'aqua', 'violet',];
+//export const COLORS = ['red', 'yellow', 'green', 'blue', 'orange', 'violet', 'aqua',];
+export const COLORS = ['#e53935', '#ffeb3b', '#43a047', '#1e88e5', '#ff9800', '#8e24aa', '#00acc1',];
