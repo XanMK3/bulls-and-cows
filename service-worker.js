8e3f4bdd598b2c0737b83e63aa84db9a9c2d8ba3
Alexander Novikov <a.e.novikov.mk3@gmail.com>
2019-07-28 22:22:55 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE = 'bulls&cows-v1.0.0-beta11';
+/* eslint-disable */
+const CACHE = 'bulls&cows-v1.0.0';
 const precacheFiles = [
     './',
     './main.css',
@@ -6,30 +7,24 @@ const precacheFiles = [
     './assets/sprite.svg',
 ];
 
-self.addEventListener('install', function (e) {
+self.addEventListener('install', (e) => {
     e.waitUntil(
-        caches.open(CACHE).then(function (cache) {
-            return cache.addAll(precacheFiles);
-        })
+        caches.open(CACHE).then(cache => cache.addAll(precacheFiles)),
     );
 });
 
-self.addEventListener('activate', function (e) {
+self.addEventListener('activate', (e) => {
     e.waitUntil(
-        caches.keys().then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
-                if (key !== CACHE) {
-                    return caches.delete(key);
-                }
-            }));
-        })
+        caches.keys().then(keyList => Promise.all(keyList.map((key) => {
+            if (key !== CACHE) {
+                return caches.delete(key);
+            }
+        }))),
     );
 });
 
-self.addEventListener('fetch', function (e) {
+self.addEventListener('fetch', (e) => {
     e.respondWith(
-        caches.match(e.request).then(function (response) {
-            return response || fetch(e.request);
-        })
+        caches.match(e.request).then(response => response || fetch(e.request)),
     );
 });
